eb80ba30e12239f90da91171261934ed7f5350b8
João Victor <joao52304@outlook.com.br>
2025-10-29 10:33:09 -0300

    Adicionando pasta TransacaoController com comando HTTP/POST/Transacao

---
+++ TransacaoControllers/TransacaoController.cs
@@ -0,0 +1,31 @@
+﻿using Desafio_Itau_backend_C_.DTO;
+using Desafio_Itau_backend_C_.Models;
+using Desafio_Itau_backend_C_.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+
+[ApiController]
+[Route("api/Controller")]
+public class TransacaoController : ControllerBase
+{
+    private readonly TransacaoService _service;
+
+    public TransacaoController(TransacaoService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CriarTransacao([FromBody] TransacaoDTO dTO)
+    {
+        var request = new TransacaoRequest(dTO.Valor, dTO.DataHora);
+        await _service.CriarTransacao(request);
+        return Ok("Transacao criada com sucesso");
+
+    }
+}
+
+    
+
